85bfa42aac5af63088552f71b8ff9f7ac5fb0264
Victor Yohanna <victoryohanna@gmail.com>
2022-12-01 10:48:15 +0100

    modified productlist component

--- src/components/dashboard/ProductList.jsx
+++ src/components/dashboard/ProductList.jsx
@@ -5,16 +5,30 @@ import { db } from "../../firebase";
 import "../styles/productlist.css";
 
 const ProductsList = () => {
+
+  const [data, setData] = useState([])
+
   useEffect(() => {
     const fetchData = async () => {
-      const querySnapshot = await getDocs(collection(db, "farmers"));
+
+      let list = []
+      const querySnapshot = await getDocs(collection(db, "products"));
       querySnapshot.forEach((doc) => {
+
+       list.push({id: doc.id, ...doc.data()})
         // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data()); 
+        // console.log(doc.id, " => ", doc.data()); 
       });
+
+      setData(list)
+      // console.log(list)
     };
+    
     fetchData() 
   }, []);
+
+  console.log(data)
+  
   return (
     <div className="container-fluid px-4">
       <div className="card card-main">
@@ -23,7 +37,8 @@ const ProductsList = () => {
             <table className="table">
               <thead>
                 <tr>
-                  <th>Product Name</th>
+                  <th></th>
+                  <th>Product Name</th> 
                   <th>Category</th>
                   <th>Date</th>
                   <th>Quantity</th>
@@ -32,13 +47,21 @@ const ProductsList = () => {
               </thead>
 
               <tbody>
-                <tr>
+                {data.map((item, i)=>{
+                  return(
+                    <tr>
+                      <td className="table-row">
+                        <img src={item.data.imageUrl} alt={item.data.productName} />
+                      </td>
+                  <td className="table-row">{item.data.productName}</td>
                   <td className="table-row"></td>
                   <td className="table-row"></td>
-                  <td className="table-row"></td>
-                  <td className="table-row"></td>
+                  <td className="table-row">{item.data.quantity}</td>
                   <td className="table-row"></td>
                 </tr>
+                  )
+                })}
+                
               </tbody>
             </table>
           </div>
