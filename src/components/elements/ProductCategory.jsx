192052aa9272a209fc88a60485a6ca9a124763f5
Victor Yohanna <victoryohanna@gmail.com>
2022-12-08 15:06:37 +0100

    add prodcuct component

---
+++ src/components/elements/ProductCategory.jsx
@@ -0,0 +1,14 @@
+
+const ProductCategory = () =>{
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4 border">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCategory
